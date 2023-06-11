ed8657b7d48819a126c8748bd0ae4400175f4544
Alejandro Rivera <124119414+AlejandroRivera2306@users.noreply.github.com>
2023-06-11 21:00:06 +0000

    version1

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy,ForeignKey
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +10,69 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Personaje %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_planeta = db.Column(db.Integer, ForeignKey('planeta.id'))
+    name_planeta = db.Column(db.String(250), ForeignKey('planeta.name'), nullable=False)
+    id_personaje = db.Column(db.Integer, ForeignKey('personaje.id'))
+    name_personaje = db.Column(db.String(250), ForeignKey('personaje.name'), nullable=False)
+    id_favorito = db.Column(db.Integer, ForeignKey('usuario.id'))
+
+    def __repr__(self):
+        return '<Favorito %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
             # do not serialize the password, its a security breach
         }
